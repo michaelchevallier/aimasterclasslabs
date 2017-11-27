a9e5ba105ee8acdc8ee5b33c5b967ab5b4e575f7
Michael CHEVALLIER <mchevall@e1r11p7.42.fr>
2017-11-27 17:40:26 +0100

    zololo

--- models.py
+++ models.py
@@ -5,9 +5,18 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc0 = nn.Linear(28*28, 27)
+        self.conv0 = nn.Conv2d(1, 4, 3, padding=1)
+        self.fc1 = nn.Linear(28*28*4, 27)
+        # self.fc1 = nn.Conv2d(4, 27, 5)
 
     def forward(self, x):
-        x = self.fc0(x.view(x.size(0), -1))
-        
-        return F.log_softmax(x)
+		x = F.relu(self.conv0(x))
+		# import pdb;pdb.set_trace()
+		x = self.fc1(x.view(x.size(0), 4 * 28 * 28))
+		return F.log_softmax(x)
+    	# x = F.relu(self.fc0(x))
+    	# x = F.relu(self.fc1(x))
+       	# return x
+    	# x = F.relu(self.fc0(x))
+    	# x =F.max_pool2d(F.relu(self.fc0(x)), (2, 2))
+        # return x
